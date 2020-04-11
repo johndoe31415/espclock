520dcd99b879252c15b64fef19e946f5222a6298
Johannes Bauer <joe@johannes-bauer.com>
2020-04-11 10:15:21 +0200

    Implemented DCF77 replacement clock
    Now generating the DCF77 signal and sending it out via GPIO and to the
    status LED as well. Actually worked on the first try! I cannot believe
    it.

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ import network
 import time
 import machine
 import socket
+import machine
 from UNTPClient import UNTPClient
 from UDateTime import UDateTime
 from Configuration import configuration
@@ -36,6 +37,8 @@ class Clock():
 		self._last_sync = None
 		self._offset = 0
 		self._dcfgen = DCF77Generator()
+		self._dcfpin = machine.Pin(15, machine.Pin.OUT)
+		self._dcfled = machine.Pin(2, machine.Pin.OUT)
 
 	def _init_wifi(self):
 		self._wifi = network.WLAN(network.STA_IF)
@@ -54,18 +57,36 @@ class Clock():
 		# hopefully won't drift that much)
 		return (self._last_sync is not None) and (self._last_sync < 86400)
 
-	def _update_display(self):
-		if self._time_valid():
-			now_utc_timet = self._now()
-			now_local = UDateTime.unix_timet_to_local_time_tuple(now_utc_timet, UDateTime.tz_europe_berlin)
-			print("Update", now_local)
-		else:
-			print("Time not valid (yet).")
+	def _dcf77_interrupt(self, arg):
+		if not self._time_valid():
+			return
+
+		now_utc_timet = self._now()
+		now_local = UDateTime.unix_timet_to_local_time_tuple(now_utc_timet, UDateTime.tz_europe_berlin)
+		hour = now_local[3]
+		minute = now_local[4]
+		second = now_local[5]
+		bit = self._dcfgen.get_bit(second)
+		if second == 59:
+			next_hour_minute = ((hour * 60) + minute + 2) % 1440
+			hour = next_hour_minute // 60
+			minute = next_hour_minute % 60
+			self._dcfgen.set_time(hour, minute)
+
+		if bit is not None:
+			self._dcfpin.value(1)
+			self._dcfled.value(1)
+			if bit == 0:
+				time.sleep(0.100)
+			else:
+				time.sleep(0.200)
+			self._dcfpin.value(0)
+			self._dcfled.value(0)
 
 	def run(self):
 		self._init_wifi()
+		machine.Timer(1).init(mode = machine.Timer.PERIODIC, period = 1000, callback = self._dcf77_interrupt)
 		while True:
-			self._update_display()
 			if self._need_sync():
 				current_timet = self._ntp.sync()
 				if current_timet is not None:
